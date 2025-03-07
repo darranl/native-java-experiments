e198ff86b7b3db4cbce9c7cc33fd8d63a65c29f1
Darran A Lofthouse <darran@lofthouse.me.uk>
2025-03-07 09:02:06 +0000

    Ensure LD_LIBRARY_PATH is set as installed libraries do not contain path information and instead rely on ldd.

--- simple-jni/run-app.sh
+++ simple-jni/run-app.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+export LD_LIBRARY_PATH=/home/darranl/local/lib
+
 java --class-path=target/simple-jni-0.0.1-SNAPSHOT.jar \
     -Djava.library.path=/home/darranl/local/lib \
     dev.lofthouse.App
